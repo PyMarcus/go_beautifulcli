3fd44e4e52be92611c9281a1be82bd132829f9dc
PyMarcus <marcus-v@outlook.com>
2023-07-25 20:43:45 -0300

    build functionality of ping

--- cmd/ping.go
+++ cmd/ping.go
@@ -1,11 +1,13 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"time"
+	"net"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
@@ -14,15 +16,45 @@ var (
 	urlPath string
 )
 
+func ping(url string){
+	fmt.Println("Request to: ", url)
+
+	req, err := http.NewRequest("HEAD", "http://" + url, nil)
+
+	if err != nil{
+		fmt.Println(err)
+	}
+
+	response, err := client.Do(req)
+
+	if err != nil{
+		fmt.Println(err)
+	}
+
+	fmt.Println("Status code: ", response.StatusCode)
+}
+
+var client http.Client 
+
 // PingCmd represents the ping command
 var PingCmd = &cobra.Command{
 	Use:   "ping",
 	Short: "A ping command that make something",
 	Long: `Command for use ping, like something how ping-pong game`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(urlPath)
 
-		fmt.Println("ping called")
+		client = http.Client{
+			Transport: &http.Transport{
+				DialContext: (&net.Dialer{
+					Timeout:   2 * time.Second,
+					KeepAlive: 30 * time.Second,
+					DualStack: true,
+				}).DialContext,
+			},
+		}
+
+		ping(urlPath)
+		
 	},
 }
 
